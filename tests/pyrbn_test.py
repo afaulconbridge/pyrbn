550b451c6944ba293787cfcb03c04eedf604e01c
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-12-28 19:11:50 +0000

    update tests

--- tests/pyrbn_test.py
+++ tests/pyrbn_test.py
@@ -8,7 +8,45 @@ def test_from_random():
     rbn = pyrbn.RBN.from_random(rng, 5)
     assert rbn is not None
     assert rbn.n == 5
-    print(rbn)
-    print(rbn.next_state())
-    print(rbn.find_cycle())
-    1 / 0
+
+
+def test_next_state():
+    rbn = pyrbn.RBN(
+        (True, False, False, False, True),
+        ((4, 0), (0, 4), (2, 3), (1, 2), (3, 1)),
+        [
+            {
+                (True, True): True,
+                (True, False): True,
+                (False, True): True,
+                (False, False): False,
+            },
+            {
+                (True, True): False,
+                (True, False): False,
+                (False, True): True,
+                (False, False): True,
+            },
+            {
+                (True, True): False,
+                (True, False): False,
+                (False, True): False,
+                (False, False): False,
+            },
+            {
+                (True, True): True,
+                (True, False): False,
+                (False, True): False,
+                (False, False): False,
+            },
+            {
+                (True, True): False,
+                (True, False): False,
+                (False, True): False,
+                (False, False): False,
+            },
+        ],
+    )
+    next_state = rbn.next_state()
+    assert len(next_state) == 5
+    assert next_state == (True, False, False, False, False)
