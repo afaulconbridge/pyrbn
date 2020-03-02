8a47f353c2ef2224dab26d6c4c7c70d4849149c5
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-03-02 16:15:47 +0000

    fix tests for new structure

--- tests/pyrbn_test.py
+++ tests/pyrbn_test.py
@@ -16,38 +16,13 @@ def test_next_state():
     rbn = pyrbn.RBN(
         (True, False, False, False, True),
         ((4, 0), (0, 4), (2, 3), (1, 2), (3, 1)),
-        [
-            {
-                (True, True): True,
-                (True, False): True,
-                (False, True): True,
-                (False, False): False,
-            },
-            {
-                (True, True): False,
-                (True, False): False,
-                (False, True): True,
-                (False, False): True,
-            },
-            {
-                (True, True): False,
-                (True, False): False,
-                (False, True): False,
-                (False, False): False,
-            },
-            {
-                (True, True): True,
-                (True, False): False,
-                (False, True): False,
-                (False, False): False,
-            },
-            {
-                (True, True): False,
-                (True, False): False,
-                (False, True): False,
-                (False, False): False,
-            },
-        ],
+        (
+            (False, True, True, True),
+            (True, True, False, False),
+            (False, False, False, False),
+            (False, False, False, True),
+            (False, False, False, True),
+        ),
     )
     next_state = rbn.next_state()
     assert len(next_state) == 5
