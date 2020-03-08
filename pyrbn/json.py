288750647d3c29119b2d794362e7a09d066fdb8f
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-03-08 14:21:56 +0000

    filled out docs for json

--- pyrbn/json.py
+++ pyrbn/json.py
@@ -14,4 +14,8 @@ def rbn_to_json(rbn):
 
 
 def json_to_rbn(data, clzz=RBN):
+    """
+    Use like this:
+    json.loads(data, object_hook=json_to_rbn)
+    """
     return RBN(data["states"], data["inputs"], data["funcs"])
