e6abc076bf0b0d7d63993a7f05c74622073da346
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-03-25 16:49:42 +0000

    use internal structure abstraction

--- pyrbn/basic.py
+++ pyrbn/basic.py
@@ -3,23 +3,23 @@ import itertools
 from typing import List, Sequence, Tuple
 
 
-@functools.total_ordering
-class RBNBasic:
-    states: Tuple[bool]
+class BooleanNetworkStructure:
+    # TODO use __new__ and weakref to prevent duplicates
+    # see https://docs.python.org/3/reference/datamodel.html#object.__new__
+    # see https://docs.python.org/3/library/weakref.html#weakref.WeakValueDictionary
     inputs: Tuple[Tuple[int]]
     funcs: Tuple[Tuple[bool]]
+    n: int
+    k: int
     pows: List[int] = []
 
     __hash = None  # store hash once calculated
     __key = ()  # store common key used for equality, ordering, hashing
 
     def __init__(
-        self,
-        states: Sequence[bool],
-        inputs: Sequence[Sequence[int]],
-        funcs: Sequence[Sequence[bool]],
+        self, inputs: Sequence[Sequence[int]], funcs: Sequence[Sequence[bool]]
     ):
-        self.n = len(states)
+        self.n = len(inputs)
         assert self.n == len(inputs)
         assert self.n > 0
         self.k = len(inputs[0])
@@ -30,8 +30,6 @@ class RBNBasic:
         assert 2 ** self.k == max((len(x) for x in funcs)), "funcs must be 2**k long"
         assert 2 ** self.k == min((len(x) for x in funcs)), "funcs must be 2**k long"
 
-        # TODO sort nodes on creation
-        self.states = tuple(states)
         self.inputs = tuple(map(tuple, inputs))
         self.funcs = tuple(map(tuple, funcs))
 
@@ -39,16 +37,109 @@ class RBNBasic:
         while self.k >= len(self.pows):
             self.pows.append(2 ** (len(self.pows)))
 
-        self.__key = (self.states, self.inputs, self.funcs)
+        self.__key = (self.inputs, self.funcs)
 
     @classmethod
     def from_random(clzz, rng, n=5, k=2):
-        states = [rng.random() >= 0.5 for i in range(n)]
         inputs = tuple(zip(*(list(rng.sample(range(n), n)) for i in range(k))))
         funcs = tuple(
             (tuple((rng.random() >= 0.5 for j in range(2 ** k))) for i in range(n))
         )
-        return clzz(states, inputs, funcs)
+        return clzz(inputs, funcs)
+
+    def next_state(self, state):
+        return tuple(
+            (
+                self.funcs[i][
+                    sum(
+                        (
+                            self.pows[j]
+                            for j in range(self.k)
+                            if state[self.inputs[i][j]]
+                        )
+                    )
+                ]
+                for i in range(self.n)
+            )
+        )
+
+    def __eq__(self, other):
+        """
+        Will only return true if nodes have the same order
+        i.e. does *NOT* respect isomorphism
+        """
+        if self is other:
+            return True
+
+        return self.__key == other.__key
+
+    def __lt__(self, other):
+        """
+        Will only work as intended if nodes have the same order
+        i.e. does *NOT* respect isomorphism
+        """
+        if self is other:
+            return False
+
+        return self.__key < other.__key
+
+    def __hash__(self):
+        """
+        Will only work as intended if nodes have the same order
+        i.e. does *NOT* respect isomorphism
+        """
+        if not self.__hash:
+            self.__hash = hash(self.__key)
+
+        return self.__hash
+
+    def __repr__(self):
+        return "{}({}, {})".format(type(self).__name__, self.inputs, self.funcs)
+
+
+@functools.total_ordering
+class RBNBasic:
+    # TODO use __new__ and weakref to prevent duplicates
+    # see https://docs.python.org/3/reference/datamodel.html#object.__new__
+    # see https://docs.python.org/3/library/weakref.html#weakref.WeakValueDictionary
+    states: Tuple[bool]
+    struct: BooleanNetworkStructure
+
+    __hash = None  # store hash once calculated
+    __key = ()  # store common key used for equality, ordering, hashing
+
+    def __init__(
+        self,
+        states: Sequence[bool],
+        inputs: Sequence[Sequence[int]],
+        funcs: Sequence[Sequence[bool]],
+    ):
+        self.states = tuple(states)
+        self.struct = BooleanNetworkStructure(inputs, funcs)
+
+        self.__key = (self.states, self.struct)
+
+    @classmethod
+    def from_random(clzz, rng, n=5, k=2):
+        states = [rng.random() >= 0.5 for i in range(n)]
+        struct = BooleanNetworkStructure.from_random(rng)
+        return clzz(states, struct.inputs, struct.funcs)
+
+    @property
+    def n(self):
+        return self.struct.n
+
+    @property
+    def k(self):
+        return self.struct.k
+
+    @property
+    def inputs(self):
+        return self.struct.inputs
+
+    @property
+    def funcs(self):
+        return self.struct.funcs
 
     def __eq__(self, other):
         """
@@ -88,20 +179,7 @@ class RBNBasic:
     def next_state(self, state=None):
         if state is None:
             state = self.states
-        return tuple(
-            (
-                self.funcs[i][
-                    sum(
-                        (
-                            self.pows[j]
-                            for j in range(self.k)
-                            if state[self.inputs[i][j]]
-                        )
-                    )
-                ]
-                for i in range(self.n)
-            )
-        )
+        return self.struct.next_state(state)
 
     def get_path_and_cycle(self, state=None):
         if state is None:
